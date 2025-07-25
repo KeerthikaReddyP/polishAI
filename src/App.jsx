abd4106a0124e17ddc06d40552523ab190040163
Keerthika Reddy <keerthikareddy267@gmail.com>
2025-07-25 09:56:15 +0530

    Centered a div😭

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import './App.css'
 function App() {
 
   return (
-    <>
+    <div className='flex items-center justify-center h-screen'>
       <hgroup>
-        <h1 className="bg-amber-600 text-3xl font-bold">Hello</h1>
+        <h1 className="bg-amber-600 text-3xl font-bold">Polish-AI</h1>
       </hgroup>
-    </>
+    </div>
   )
 }
 
